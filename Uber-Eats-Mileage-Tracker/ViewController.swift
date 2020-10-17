4883123f5c32fe52c6291a1591c6ed3de6dda18d
Taylor Conkle <t.conkle01@gmail.com>
2020-10-17 12:57:01 -0500

    Initial Commit

---
+++ Uber-Eats-Mileage-Tracker/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Uber-Eats-Mileage-Tracker
+//
+//  Created by Taylor Conkle on 10/17/20.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
